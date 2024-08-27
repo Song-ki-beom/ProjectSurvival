cf7e620ec17ae29c1beaae8556e0e60a8133604a
kiboem <63495505+Song-ki-beom@users.noreply.github.com>
2024-08-27 17:16:38 +0900

    PickUp 상속 1

--- Source/ProjectSurvival/Build/CStructure.h
+++ Source/ProjectSurvival/Build/CStructure.h
@@ -24,11 +24,11 @@ public:
 	UFUNCTION(NetMulticast, Reliable)
 		void BroadcastDestroyPreviewBox();
 
-	//virtual void BeginFocus() override;
-	//virtual void EndFocus() override;
-	//virtual void BeginInteract() override; //인터렉션까지의 타이머 설정
-	//virtual void EndInteract() override;
-	//virtual void Interact(class ACSurvivor* PlayerCharacter) override; // 인터렉트가 준비되면 바로 Interact 시작 
+	virtual void BeginFocus() override;
+	virtual void EndFocus() override;
+	virtual void BeginInteract() override; //인터렉션까지의 타이머 설정
+	virtual void EndInteract() override;
+	virtual void Interact(class ACSurvivor* PlayerCharacter) override; // 인터렉트가 준비되면 바로 Interact 시작 
 
 protected:
 	void SaveOriginMaterial();
